a5c7dde009623de4860a58ad8fb200cfb3150917
João Dias <joaovitorelf@gmail.com>
2025-06-24 14:59:37 -0300

    Criando uma order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,8 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 import uploadConfig from './config/multer'
@@ -26,15 +28,18 @@ router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
 
-//-- ROTAS CATEGORY
+//-- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle )
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle )
 
-//-- ROTAS PRODUCT
+//-- ROTAS PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
+
+// -- ROTAS ORDER --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
 
 
 export { router }; 
