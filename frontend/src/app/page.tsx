b82d3ae3c7998fc6a4bb0ca525cd040ffde115cd
João Dias <joaovitorelf@gmail.com>
2025-07-01 10:48:38 -0300

    Realizando o login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,50 @@
+import { redirect } from 'next/navigation'
 import styles from './page.module.scss'
 import logoImg from '/public/menu-logo.svg'
+import { api } from '@/services/api'
 
 import Link from 'next/link'
 
-
 export default function Page() {
+
+  async function handleLogin(formData: FormData) {
+    "use server"
+
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    if (email === "" || password === "") {
+      return
+    }
+
+    try {
+
+      const response = await api.post("/session", {
+        email,
+        password
+      })
+
+      if (!response.data.token) {
+        return
+      }
+
+      console.log(response)
+
+    } catch (err) {
+      console.log(err)
+      return
+    }
+
+    redirect("/dashboard")
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
         <h1 className={styles.title}>E-MENU</h1>
 
         <section className={styles.login}>
-          <form action="">
+          <form action={handleLogin}>
             <input
               type="email"
               required
