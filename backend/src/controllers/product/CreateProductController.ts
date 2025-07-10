58d451ac92e0eb3bc99b51c339eb640a09c36e20
João Dias <joaovitorelf@gmail.com>
2025-07-10 14:41:18 -0300

    Ajustando o envio de imagens para o DEPLOY

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -1,19 +1,44 @@
-import {Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { CreateProductService } from '../../services/product/CreateProductService'
+import { UploadedFile } from 'express-fileupload'
+
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+})
 
 
-class CreateProductController{
-  async handle(req: Request, res: Response){
+class CreateProductController {
+  async handle(req: Request, res: Response) {
     const { name, price, description, category_id } = req.body;
 
     const createProductService = new CreateProductService();
 
-    if(!req.file){
-      throw new Error("error upload file")
-    }else{
+    if (!req.files || Object.keys(req.files).length === 0) {
+      throw new Error("error upload file image")
+    } else {
 
-      const { originalname, filename: banner } = req.file;
+      // const { originalname, filename: banner } = req.file;
 
+      const file: UploadedFile = req.files['file']
+
+      const resultFile = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({}, function (error, result) {
+          if (error) {
+            reject(error)
+            return
+          }
+
+          resolve(result)
+        }).end(file.data)
+      })
+
+      return res.json({})
+
+      /*
       const product = await createProductService.execute({
         name,
         price,
@@ -21,8 +46,9 @@ class CreateProductController{
         banner,
         category_id
       });
-  
+
       return res.json(product)
+      */
     }
 
   }
