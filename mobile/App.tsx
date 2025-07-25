476415f2dcba663bb47ac27db7a07bd1eb21871c
João Dias <joaovitorelf@gmail.com>
2025-07-25 16:32:12 -0300

    Configurando a navegação do app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,12 @@
 import { StyleSheet, View, StatusBar } from 'react-native';
-import SignIn from './src/pages/SignIn';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View>
+    <NavigationContainer>
       <StatusBar backgroundColor={'#1d1d2e'} barStyle='light-content' translucent={false} />
-      <SignIn />
-    </View>
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
