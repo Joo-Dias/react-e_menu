90c62689460b4ea3b12419453318e73b7bdc51f8
João Dias <joaovitorelf@gmail.com>
2025-07-25 16:11:48 -0300

    Criando o projeto mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View, StatusBar } from 'react-native';
+import SignIn from './src/pages/SignIn';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar backgroundColor={'#1d1d2e'} barStyle='light-content' translucent={false} />
+      <SignIn />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
