766da6c9a7df72c74b78ef665bf21bb93d82fba4
João Dias <joaovitorelf@gmail.com>
2025-07-30 10:03:07 -0300

    feat(login): funcionalidade do login

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
 import React, { useState, createContext, ReactNode } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { api } from '../services/api'
 
 type AuthContextData = {
     user: UserProps;
     isAuthenticated: boolean;
-
+    signIn: (credentials: SignInProps) => Promise<void>
 }
 
 type UserProps = {
@@ -17,6 +20,11 @@ type AuthProviderProps = {
     children: ReactNode
 }
 
+type SignInProps = {
+    email: string;
+    password: string;
+}
+
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
@@ -27,10 +35,46 @@ export function AuthProvider({ children }: AuthProviderProps) {
         token: ''
     })
 
+    const [loadingAuth, setLoadingAuth] = useState(false)
+
     const isAuthenticated = !!user.name
 
+    async function signIn({ email, password }: SignInProps) {
+        setLoadingAuth(true)
+
+        try {
+
+            const response = await api.post('/session', {
+                email,
+                password
+            })
+
+            const { id, name, token } = response.data
+
+            const data = {
+                ...response.data
+            }
+
+            await AsyncStorage.setItem('@emenu', JSON.stringify(data))
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            setUser({
+                id,
+                name,
+                email,
+                token
+            })
+
+            setLoadingAuth(false)
+        } catch (err) {
+            console.log('Erro ao acessar', err)
+            setLoadingAuth(false)
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
             {children}
         </AuthContext.Provider>
     )
