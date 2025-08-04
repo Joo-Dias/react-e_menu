600735badedc266d415416d49abc8c6c0511d485
João Dias <joaovitorelf@gmail.com>
2025-08-04 16:04:01 -0300

    feat(order): receber parametros

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -2,10 +2,26 @@
 import react from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+import { useRoute, RouteProp } from '@react-navigation/native'
+
+type RouteDetailsParams = {
+    Order: {
+        number: string | number;
+        order_id: string | number;
+    }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailsParams, 'Order'>;
+
 export default function Order() {
+    const route = useRoute<OrderRouteProps>();
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Pedido</Text>
+            <Text style={styles.title}>
+                {route.params.number}
+            </Text>
         </View>
     )
 }
