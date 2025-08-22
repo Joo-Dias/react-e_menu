8d1cb29d191abf20d232fb24c194258045ad1f45
João Dias <joaovitorelf@gmail.com>
2025-08-22 16:31:05 -0300

    feat(products): listar produtos

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -107,6 +107,11 @@ export default function Order() {
         setCategorySelected(item)
     }
 
+    // Função para mudar o produto que está selecionado
+    function handleChangeProduct(item: ProductProps) {
+        setProductSelected(item)
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -129,7 +134,7 @@ export default function Order() {
             )}
 
             {products.length !== 0 && (
-                <TouchableOpacity style={styles.input}>
+                <TouchableOpacity style={styles.input} onPress={() => setModalProductVisible(true)}>
                     <Text style={{ color: '#FFF' }}>{productSelected?.name}</Text>
                 </TouchableOpacity>
             )}
@@ -164,7 +169,19 @@ export default function Order() {
                 <ModalPicker
                     handleCloseModal={() => setModalCategoryVisible(false)}
                     options={category}
-                    selectedItem={() => { handleChangeCategory }}
+                    selectedItem={handleChangeCategory}
+                />
+            </Modal>
+
+            <Modal
+                transparent={true}
+                visible={modalProductVisible}
+                animationType='fade'
+            >
+                <ModalPicker
+                    handleCloseModal={() => setModalProductVisible(false)}
+                    options={products}
+                    selectedItem={handleChangeProduct}
                 />
             </Modal>
         </View>
